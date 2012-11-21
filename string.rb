3065bd0f031020a8c1d1a1cadef832a6523841f1
Azitabh Ajit <azitabh@gmail.com>
2012-11-22 01:50:02 +0530

    indenting

--- string.rb
+++ string.rb
@@ -1,10 +1,10 @@
 #Extending string class
 class String
 	#ifentifying all popular strings referred as boolean and converting it to boolean
-    def to_bool
-        return true   if self == true   || self =~ (/(true|t|yes|y|1)$/i)
-        return false  if self == false  || self =~ (/(false|f|no|n|0)$/i)
-        return false
-    end
+	def to_bool
+		return true   if self == true   || self =~ (/(true|t|yes|y|1)$/i)
+		return false  if self == false  || self =~ (/(false|f|no|n|0)$/i)
+		return false
+	end
 end
 
